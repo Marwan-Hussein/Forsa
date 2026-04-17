66c6963cc4758ef835becc89a7f2cf71e27cfd7e
marwan hussein <marawanlabana649@gmail.com>
2026-04-17 03:14:46 +0200

    Revert "Adjust ENUMS structure & User Roles & Create Entites"
    This reverts commit 7101994c11bf8b44353432ecebcd82b0430ef405.

---
+++ Domain/ENUMs/SharedEnums.cs
@@ -0,0 +1,32 @@
+﻿namespace Domain.ENUMs
+{
+    public enum Roles
+    {
+        guest = 1,
+        Attendee , 
+        Owner, 
+        Admin 
+    }
+
+    public enum NotificationType
+    {
+        GeneralAlert = 1,
+        BookingConfirmation = 2,
+        EventUpdate = 3,
+        CertificateReady = 4
+    }
+
+    public enum DeliveryMethod
+    {
+        Email = 1,
+        WhatsApp = 2,
+        Both = 3
+    }
+
+    public enum NotificationStatus
+    {
+        Pending = 1,
+        Sent = 2,
+        Failed = 3
+    }
+}
