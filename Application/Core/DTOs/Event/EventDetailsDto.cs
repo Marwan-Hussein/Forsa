2313384ecb8e394d4dac6b8749a9b41eda2022ce
mariam Ehab <mariamehab135721@gmail.com>
2026-04-23 16:39:20 +0200

    Add the DTOs and automapper profiles for booking and event

---
+++ Application/Core/DTOs/Event/EventDetailsDto.cs
@@ -0,0 +1,17 @@
+namespace Application.Core.DTOs.Event
+{
+    public class EventDetailsDto
+    {
+        public int EventId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Category { get; set; }
+        public double TicketPrice { get; set; }
+        public int TotalTickets { get; set; }
+        public int RemainingTickets { get; set; }
+        public string Status { get; set; }  // Enum EventStatus
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Place { get; set; }
+    }
+}
