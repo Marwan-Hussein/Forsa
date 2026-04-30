1658c354ea5bb25e24f7839fe665149c2ac9426a
marwan hussein <marawanlabana649@gmail.com>
2026-05-01 02:49:54 +0300

    OTP Validator

---
+++ Application/Validators/OTPValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+namespace Application.Validators
+{
+    public class OTPValidator : AbstractValidator<string>
+    {
+        public OTPValidator() { 
+            RuleFor(otp => otp)
+                .NotEmpty()
+                .WithMessage("OTP is required.")
+
+                .Length(5)
+                .WithMessage("OTP must be 5 digits.")
+                
+                .Must(otp => otp.All(char.IsDigit))
+                .WithMessage("OTP must contain only digits.");
+        }
+    }
+}
