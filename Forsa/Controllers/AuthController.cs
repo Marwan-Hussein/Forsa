ab68bc410b331819474053adf6a6491fea457491
MazenAbdelateef <mazenkahraba502@gmail.com>
2026-05-01 04:35:32 +0300

    adding controller method to handle the external login

--- Forsa/Controllers/AuthController.cs
+++ Forsa/Controllers/AuthController.cs
@@ -1,6 +1,9 @@
 ﻿using Application.Core.DTOs.Auth;
 using Application.Core.Interfaces.Auth;
+using Domain.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace Forsa.Controllers
 {
@@ -9,10 +12,14 @@ namespace Forsa.Controllers
     public class AuthController : ControllerBase
     {
         private readonly IAuthService _authService;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IExternalAuth _externalAuth;
 
-        public AuthController(IAuthService authService)
+        public AuthController(IAuthService authService,IExternalAuth externalAuth , SignInManager<ApplicationUser> signInManager)
         {
             _authService = authService;
+            _signInManager = signInManager; 
+            _externalAuth = externalAuth;
         }
 
         [HttpPost("register")]
@@ -50,5 +57,47 @@ namespace Forsa.Controllers
                 return StatusCode(500, "An error occurred while logging in");
             }
         }
+
+
+        // external login endpoints 
+        [HttpGet("external-login")]
+        public IActionResult ExternalLogin(string provider) {
+            var redirectUrl = Url.Action(nameof(ExternalCallBack),"Auth");
+            var properties = _signInManager.ConfigureExternalAuthenticationProperties(provider , redirectUrl);
+            return Challenge(properties,provider);
+        }
+
+        [HttpGet("external-callback")]
+        public async Task<IActionResult> ExternalCallBack() { 
+
+            var info = await _signInManager.GetExternalLoginInfoAsync();
+
+            if (info == null) {
+                return StatusCode(500, "Error loading external information");
+            }
+
+            var email = info.Principal.FindFirstValue(ClaimTypes.Email);
+
+            if (string.IsNullOrEmpty(email)) { 
+                return StatusCode(500, "An email is required from provider");
+            }
+
+            var name = info.Principal.FindFirstValue(ClaimTypes.Name);
+
+            var authDto = new ExternalAuthDto
+            {
+
+                Provider = info.LoginProvider,
+                ProviderKey = info.ProviderKey,
+                Email = email,
+                Name = name
+            };
+
+            var userDto = await _externalAuth.ProcessExternalLoginAsync(authDto);
+
+            return Ok(userDto);
+
+        }
+
     }
 }
