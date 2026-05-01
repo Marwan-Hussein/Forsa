2b746d6881b992e2e5f10fdad097983efb3063b8
marwan hussein <marawanlabana649@gmail.com>
2026-05-01 03:43:03 +0300

    Email Service and user Secrets

---
+++ Application/Services/Auth/EmailService.cs
@@ -0,0 +1,29 @@
+﻿using MailKit.Net.Smtp;
+using MailKit;
+using Application.Core.Interfaces.Auth;
+using MimeKit;
+
+namespace Application.Services.Auth
+{
+    public class EmailService : IEmailService
+    {
+        public Task SendAsync(string to, string subject, string body)
+        {
+            var msg = new MimeMessage();
+            msg.From.Add(new MailboxAddress("Forsa", "no-reply@forsa.com"));
+            msg.To.Add(new MailboxAddress("",to));
+            msg.Subject = subject;
+            msg.Body = new TextPart("html")
+            {
+                Text = body
+            };
+
+            // to connect to the SMTP server and send the email
+            using var client = new SmtpClient();
+            await client.ConnectAsync("smtp.forsa.com", 587, false);
+            await client.AuthenticateAsync("no-reply@forsa.com", "yourpassword");
+            await client.SendAsync(msg);
+            await client.DisconnectAsync(true);
+        }
+    }
+}
---
+++ Application/Services/Auth/IRedisCacheService.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.Services.Auth
+{
+    public interface IRedisCacheService
+    {
+    }
+}
