d7869ac4d54a746b71ab5ef6e7217996b0a2510c
marwan hussein <marawanlabana649@gmail.com>
2026-05-08 06:31:26 +0300

    performance metric as generic query

---
+++ Application/Queries/Admin/GetPerformanceReportQuery.cs
@@ -0,0 +1,23 @@
+﻿using Application.Core.DTOs.Admin;
+using System.Runtime.CompilerServices;
+
+namespace Application.Queries.Admin
+{
+    public static class GetPerformanceReportQuery
+    {
+        public static IQueryable<PerformanceReportDTO> PerfomanceMetrics(
+            this IQueryable<PerformanceReportDTO> query, Func<PerformanceReportDTO, bool> fnc)
+        {
+            return query
+                .Where(report => report.CompletedTasks > 0)
+                .Where(report => report.AverageRatings < 2.5)
+                .Select(report => new PerformanceReportDTO
+                {
+                    CompletedTasks = report.CompletedTasks,
+                    AverageRatings = report.AverageRatings,
+                    TotalEarnings = report.TotalEarnings
+                })
+                .OrderBy(report => fnc);
+        }
+    }
+}
---
+++ Forsa/Hubs/NotificationsHub.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Forsa.Hubs
+{
+    public class NotificationsHub : Hub
+    {
+        public override async Task OnConnectedAsync()
+        {
+            var userId = Context.UserIdentifier;
+            if (!string.IsNullOrEmpty(userId))
+            {
+                await Groups.AddToGroupAsync(Context.ConnectionId, userId);
+            }
+            await base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception? exception)
+        {
+            return base.OnDisconnectedAsync(exception);
+        }
+
+    }
+}
