a9e253bc6195c30795c2c38a761fe47b02c752e5
marwan hussein <marawanlabana649@gmail.com>
2026-05-08 07:09:57 +0300

    GetPerformanceQuery

---
+++ Application/Queries/Admin/GetPerformanceQuery.cs
@@ -0,0 +1,10 @@
+﻿using Application.Core.DTOs.Admin;
+using MediatR;
+
+namespace Application.Queries.Admin
+{
+    public record GetPerformanceQuery(
+        DateTime From,
+        DateTime To
+        ): IRequest<PerformanceReportDTO>;
+}
