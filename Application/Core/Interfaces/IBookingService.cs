09f68628fdcb4cae3b3a70bf9fc57cf4f38605cd
MazenAbdelateef <mazenkahraba502@gmail.com>
2026-06-18 23:39:10 +0300

    adding creating qrImage and validate the attendee via QrCode

--- Application/Core/Interfaces/IBookingService.cs
+++ Application/Core/Interfaces/IBookingService.cs
@@ -9,5 +9,9 @@ namespace Application.Core.Interfaces
         Task<BookingResponseDto> CreateBookingAsync(CreateBookingRequestDto dto);
         Task<BookingResponseDto> GetBookingAsync(int bookingId);
         Task CancelBookingAsync(int bookingId);
+
+
+        Task<byte[]> GetTicketFromQr(int bookingId);
+        Task VerifyAttendanceViaQrCodeAsync(int eventId, string qrCode);
     }
 }
