22fdebb1a9e7bfac40d56cf9843b1bdb2519935b
marwan hussein <marawanlabana649@gmail.com>
2026-06-25 04:16:14 +0300

    Place Availabilities

--- Infrastructure/Data/Configurations/Place/PlaceAvailabilityConfiguration.cs
+++ Infrastructure/Data/Configurations/Place/PlaceAvailabilityConfiguration.cs
@@ -1,6 +1,7 @@
 using Domain.Entities.PlaceEntities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.ENUMs;
 
 namespace Infrastructure.Data.Configurations
 {
@@ -16,6 +17,18 @@ namespace Infrastructure.Data.Configurations
                    .WithMany(p => p.PlaceAvailabilities)
                    .HasForeignKey(pa => pa.PlaceId)
                    .OnDelete(DeleteBehavior.Cascade);
+            var placeAvailabilities = new List<PlaceAvailability>();
+            for(int i = 1; i < 10; i++)
+                placeAvailabilities.Add(new PlaceAvailability
+                {
+                    Id = i,
+                    Date = DateTime.Now.AddDays(i),
+                    StartTime = new TimeSpan(9, 0, 0),
+                    EndTime = new TimeSpan(17, 0, 0),
+                    Status = (PlaceStatus)(i % 6 + 1),
+                    PlaceId = i
+                });
+            builder.HasData(placeAvailabilities);
         }
     }
 }
