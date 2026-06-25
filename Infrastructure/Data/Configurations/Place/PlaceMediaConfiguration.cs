5d7a11aa704144f302fc0454954757a9b763bff1
marwan hussein <marawanlabana649@gmail.com>
2026-06-25 04:13:16 +0300

    Place Medias

--- Infrastructure/Data/Configurations/Place/PlaceMediaConfiguration.cs
+++ Infrastructure/Data/Configurations/Place/PlaceMediaConfiguration.cs
@@ -1,7 +1,7 @@
 using Domain.Entities.PlaceEntities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-
+using Domain.ENUMs;
 namespace Infrastructure.Data.Configurations
 {
     public class PlaceMediaConfiguration : IEntityTypeConfiguration<PlaceMedia>
@@ -16,6 +16,17 @@ namespace Infrastructure.Data.Configurations
                    .WithMany(p => p.PlaceMedias)
                    .HasForeignKey(pm => pm.PlaceId)
                    .OnDelete(DeleteBehavior.NoAction);
+
+            var placeMedias = new List<PlaceMedia>();
+            for(int i=1; i <= 25; i++)
+                placeMedias.Add(new PlaceMedia
+                {
+                    Id = i,
+                    PlaceId = i%10 +1,
+                    MediaType = (MediaType)(i % 2 + 1),
+                    MediaURL = $"https://forsa.com/mediaURLs/{i}.jpg",
+                });
+            builder.HasData(placeMedias);
         }
     }
 }
