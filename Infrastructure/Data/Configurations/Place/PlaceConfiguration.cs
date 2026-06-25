152216b6d5e30e9464363acd86e50bd6fd6838c5
marwan hussein <marawanlabana649@gmail.com>
2026-06-25 04:13:03 +0300

    Fake Places

--- Infrastructure/Data/Configurations/Place/PlaceConfiguration.cs
+++ Infrastructure/Data/Configurations/Place/PlaceConfiguration.cs
@@ -1,6 +1,7 @@
 using Domain.Entities.PlaceEntities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Domain.ENUMs;
 
 namespace Infrastructure.Data.Configurations
 {
@@ -16,6 +17,25 @@ namespace Infrastructure.Data.Configurations
                    .WithMany(o => o.Places)
                    .HasForeignKey(p => p.OwnerId)
                    .OnDelete(DeleteBehavior.NoAction);
+            var places = new List<Place>();
+            for (int i = 1; i <= 25; i++)
+                places.Add(new Place
+                {
+                    Id = i,
+                    Name = $"Place {i}",
+                    Location = $"Location {i}",
+                    Capacity = 100 + 10 * i,
+                    Description = $"Description for Place {i}",
+                    HourlyPrice = 10.0m + i,
+                    DailyPrice = 100.0m + i,
+                    Status = (PlaceStatus)(i % 6 + 1),
+                    FacilityName = (FacilityName)(i % 4 + 1),
+                    IsLocked = false,
+                    OwnerId = (i % 5) + 1 // Assigning owners in a round-robin fashion
+                });
+                builder.HasData(
+                    places
+                );
         }
     }
 }
