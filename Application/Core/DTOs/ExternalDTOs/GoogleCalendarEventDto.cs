e6b199155723aa566c3b39cc040867f192abf7cc
marwan hussein <marawanlabana649@gmail.com>
2026-06-28 02:52:14 +0300

    Application.DTOs.GoogleCalEvent

---
+++ Application/Core/DTOs/ExternalDTOs/GoogleCalendarEventDto.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualBasic;
+
+namespace Application.Core.DTOs.ExternalDTOs
+{
+    public class GoogleCalendarEventDto
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartTime { get; set;  }
+        public DateAndTime EndTime { get; set;  }
+    }
+}
