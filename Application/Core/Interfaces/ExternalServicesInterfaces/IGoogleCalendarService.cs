374fc7404711a74267373885056f3a957571eec3
marwan hussein <marawanlabana649@gmail.com>
2026-06-28 02:16:14 +0300

    Application.Interfaces.IGoogleCalendarService
    abstracted CRUD services

---
+++ Application/Core/Interfaces/ExternalServicesInterfaces/IGoogleCalendarService.cs
@@ -0,0 +1,11 @@
+﻿using Google.Apis.Calendar.v3;
+
+namespace Application.Core.Interfaces.ExternalServicesInterfaces
+{
+    public interface IGoogleCalendarService
+    {
+        Task<string> CreateEventAsync(GoogleCalendarEventDto eventDto, CancellationToken cancellationToken);
+        Task UpdateEventAsync(string googleEventId, GoogleCalendarEventDto eventDto, CancellationToken cancellationToken);
+        Task DeleteEventAsync(string googleEventId, CancellationToken cancellationToken);
+    }
+}
