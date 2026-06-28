3fae66e2a13fe8ced6e97eaf33261f172f5d5899
marwan hussein <marawanlabana649@gmail.com>
2026-06-28 03:27:40 +0300

    App.settings.GoogleServicesSettings

---
+++ Application/Core/Settings/GoogleServicesSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Application.Core.Settings
+{
+    public class GoogleServicesSettings
+    {
+        public string ClientId {  get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
