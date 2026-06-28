8a4a8e50ba83e57e6b74dcfe108ddc96d5bb270e
marwan hussein <marawanlabana649@gmail.com>
2026-06-28 06:03:15 +0300

    setting the controller

---
+++ Forsa/Controllers/ExternalControllers/GoogleCalendarController.cs
@@ -0,0 +1,19 @@
+﻿using Application.Core.Interfaces.ExternalServicesInterfaces;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Forsa.Controllers.ExternalControllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GoogleCalendarController : ControllerBase
+    {
+        private readonly IGoogleCalendarService _googleCalendarService;
+        public GoogleCalendarController(IGoogleCalendarService googleCalendarService)
+        {
+            _googleCalendarService = googleCalendarService;
+        }
+
+        
+    }
+}
