7084ca91ee11841770a18653a5d7f7a6bd4270c9
marwan hussein <marawanlabana649@gmail.com>
2026-06-28 04:10:06 +0300

    Update GoogleCalendarEventDto.cs

--- Application/Core/DTOs/ExternalDTOs/GoogleCalendarEventDto.cs
+++ Application/Core/DTOs/ExternalDTOs/GoogleCalendarEventDto.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.VisualBasic;
-
 namespace Application.Core.DTOs.ExternalDTOs
 {
     public class GoogleCalendarEventDto
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public DateTime StartTime { get; set;  }
-        public DateAndTime EndTime { get; set;  }
+        public string? Location { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string? TimeZone { get; set; }
     }
 }
