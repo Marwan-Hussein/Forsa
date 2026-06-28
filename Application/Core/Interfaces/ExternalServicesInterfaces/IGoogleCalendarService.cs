46dce85a7f2345ef100f5a06e45406cb6b1e5844
marwan hussein <marawanlabana649@gmail.com>
2026-06-28 04:14:50 +0300

    Adding GetEventAsync function to the service

--- Application/Core/Interfaces/ExternalServicesInterfaces/IGoogleCalendarService.cs
+++ Application/Core/Interfaces/ExternalServicesInterfaces/IGoogleCalendarService.cs
@@ -1,11 +1,18 @@
-﻿using Application.Core.DTOs.ExternalDTOs;
+using Application.Core.DTOs.ExternalDTOs;
 
 namespace Application.Core.Interfaces.ExternalServicesInterfaces
 {
     public interface IGoogleCalendarService
     {
-        Task<string> CreateEventAsync(GoogleCalendarEventDto eventDto, CancellationToken cancellationToken);
-        Task UpdateEventAsync(string googleEventId, GoogleCalendarEventDto eventDto, CancellationToken cancellationToken);
-        Task DeleteEventAsync(string googleEventId, CancellationToken cancellationToken);
+        // creates a new event in google calendar
+        // returns the eventID of the created event
+        Task<string> CreateEventAsync(GoogleCalendarEventDto eventDto, CancellationToken cancellationToken = default);
+        Task UpdateEventAsync(string googleEventId, GoogleCalendarEventDto eventDto, CancellationToken cancellationToken = default);
+
+        Task DeleteEventAsync(string googleEventId, CancellationToken cancellationToken = default);
+
+        // retrieves an event from Google Calendar by its ID
+        // returns the CalendarEventDTO obj or null if not existed
+        Task<GoogleCalendarEventDto?> GetEventAsync(string googleEventId, CancellationToken cancellationToken = default);
     }
 }
