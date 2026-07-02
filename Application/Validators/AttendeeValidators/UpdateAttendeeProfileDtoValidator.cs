b35cc087c52bb0fc550d8c356f1b9ac89cfed570
mariam Ehab <mariamehab135721@gmail.com>
2026-07-02 15:10:04 +0300

    fix required fields in update attendee profile

--- Application/Validators/AttendeeValidators/UpdateAttendeeProfileDtoValidator.cs
+++ Application/Validators/AttendeeValidators/UpdateAttendeeProfileDtoValidator.cs
@@ -10,13 +10,6 @@ namespace Application.Validators.AttendeeValidators
             RuleFor(x => x.FullName)
                 .NotEmpty().WithMessage("Full name is required.");
 
-            RuleFor(x => x.UserName)
-                .NotEmpty().WithMessage("Username is required.")
-                .MinimumLength(3).WithMessage("Username must be at least 3 characters.");
-
-            RuleFor(x => x.Email)
-                .NotEmpty().WithMessage("Email is required.")
-                .EmailAddress().WithMessage("Please enter a valid email address.");
 
             RuleFor(x => x.PhoneNumber)
                 .NotEmpty().WithMessage("Phone number is required.");
